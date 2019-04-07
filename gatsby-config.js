e978976b1fe43694b15ae977e06b04aa5394961e
Nkosi Phillip <internap627@gmail.com>
2019-04-07 16:37:39 +0100

    metadata-description-keyword-update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Design Code`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Complete tutorials on React and React native. Learn GraphQl and contentful.`,
+    keywords: 'react, native, react course, development' ,
     author: `@gatsbyjs`,
   },
   plugins: [
